dd7c552dcd976cd8b6ef1c27516a95b3b025d872
Dasandh <sandh.david@gmail.com>
2024-04-21 15:00:33 +0200

    Add class Grid

---
+++ src/main/java/com/devoteam/grid/Grid.java
@@ -0,0 +1,26 @@
+package com.devoteam.grid;
+
+public class Grid {
+    private int width;
+    private int height;
+
+    public Grid() {
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
